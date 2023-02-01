13e2408974886d0d23e649fbb7ba0f3043f7adc2
Pedro Félix <62726193+pedrobrafel@users.noreply.github.com>
2023-02-01 16:00:03 -0300

    atualizei wdio conf

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,25 +1,35 @@
-// wdio.conf.js
-const allure = require('allure-commandline')
 const { join } = require('path')
+const allure = require('allure-commandline')
+const video = require('wdio-video-reporter')
 
 exports.config = {
-    baseUrl: 'http://localhost',
-    port: 4723,
-    path: '/wd/hub',
-    udid: "emulator-5554",
-    waitForTimeout: 999999,
+    // baseUrl: 'http://localhost',
+    // port: 4723,
+    // path: '/wd/hub',
+    // services: ['appium'],
+    user: 'pedroflix_p5KMJE',
+    key: 'TTqqup4pVhX4pkZ5FiVy',
+    services: ['browserstack'],
+    waitForTimeout: 99999,
     specs: [
         './test/specs/**/*.spec.js'
     ],
     framework: 'mocha',
     capabilities: [{
-        "platformName": "Android",
-        "appium:platformVersion": "8.0",
-        "appium:deviceName": "Pixel 2 API 26",
-        "appium:automationName": "UiAutomator2",
-        "appium:app": join(process.cwd(), './app/android/loja-ebac.apk'),
-        "appium:appWaitActivity": 'com.woocommerce.android.ui.login.LoginActivity',
-        "appium:newCommandTimeout": 999
+        // "platformName": "Android",
+        // "appium:platformVersion": "8.0",
+        // "appium:deviceName": "Pixel 2 API 26",
+        // "appium:automationName": "UiAutomator2",
+        // "appium:app": join(process.cwd(), './app/android/loja-ebac.apk'),
+        // "appium:appWaitActivity": 'com.woocommerce.android.ui.login.LoginActivity',
+        // "appium:newCommandTimeout": 999
+
+        'app': 'bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c',
+        'device': 'Samsung Galaxy S10',// Specify device and os_version for testing
+        'os_version': '9.0',        // Set other BrowserStack capabilities
+        'project': 'Meu primeiro projeto em device farm',
+        'build': 'build-1',
+        'name': 'teste_login'
     }],
     mochaOpts: {
         timeout: 999999
@@ -29,7 +39,11 @@ exports.config = {
             outputDir: 'allure-results',
             disableWebdriverStepsReporting: false,
             disableWebdriverScreenshotsReporting: true,
-        }]
+        }],
+        [video, {
+            saveAllVideos: false,       // If true, also saves videos for successful test cases
+            videoSlowdownMultiplier: 3, // Higher to get slower videos, lower for faster videos [Value 1-100]
+        }],
     ],
     onComplete: function () {
         const reportError = new Error('Could not generate Allure report')
